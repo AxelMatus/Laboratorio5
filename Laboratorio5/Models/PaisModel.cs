e2fa6f75de76f60e6de904674491ded4395ada7a
AxelMatus <axelantonio@gmail.com>
2022-05-19 23:43:56 -0600

    Agregado MVC de Pais

---
+++ Laboratorio5/Models/PaisModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Laboratorio5.Models
+{
+    public class PaisModel : Controller
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Continente { get; set; }
+        public string Idioma { get; set; }
+
+    }
+}
